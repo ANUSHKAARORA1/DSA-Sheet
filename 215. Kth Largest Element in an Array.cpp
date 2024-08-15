9f6b6899b526d20ac34fa7ca065c37f926a69889
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-08-15 11:07:36 +0530

    Create 215. Kth Largest Element in an Array.cpp

---
+++ 215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,9 @@
+// 215. Kth Largest Element in an Array
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+    return nums[n-k];
+    }
+};
