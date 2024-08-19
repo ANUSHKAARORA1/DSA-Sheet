92ec876097fb02e0afaef3c7baf8b5f9b9f2ba1e
Anushka Arora <157234167+ANUSHKAARORA1@users.noreply.github.com>
2024-08-19 13:18:40 +0530

    Update 75. Sort Colors.cpp

--- 75. Sort Colors.cpp
+++ 75. Sort Colors.cpp
@@ -2,6 +2,54 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-    }
+        // sort(nums.begin(),nums.end());
+        //m1
+        // int count0=0;
+        // int count1=0;
+        // int count2=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==0){
+        //         count0++;
+        //     }
+        //    else if(nums[i]==1)
+        //     {
+        //         count1++;
+        //     }
+        //   else {
+        //         count2++;
+        //     }
+        // }
+        // int i=0;
+        // while(count0--){
+        //     nums[i]=0;
+        //     i++;
+        // }
+        // while(count1--){
+        //    nums[i]=1;
+        //    i++;
+        // }
+        // while(count2--){
+        //     nums[i]=2;
+        //     i++;
+        // }
+        //inplace 
+        int m=0;
+        int l=0;
+        int h=nums.size()-1;
+        int i=0;
+        while(m<=h){
+            if(nums[m]==0){
+                swap(nums[m],nums[l]);
+                m++;
+                l++;
+            }
+            else if(nums[m]==1){
+                m++;
+            }
+            else{
+                swap(nums[m],nums[h]);
+                h--;
+            }
+        }
+        } 
 };
